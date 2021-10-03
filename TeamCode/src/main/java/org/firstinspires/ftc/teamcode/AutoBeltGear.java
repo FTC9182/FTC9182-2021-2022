bf992bb9b59f136581768e0edc05f38a02570735
BlueBoat74 <Animated27engineer@gmail.com>
2021-10-03 12:57:44 -0500

    Update AutoBeltGear.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoBeltGear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoBeltGear.java
@@ -1,4 +1,26 @@
 package org.firstinspires.ftc.teamcode;
 
-public class AutoBeltGear {
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+//@Autonomous(name = "AutoBeltGear", group = "Test")
+@TeleOp(name = "TestBeltGear", group = "Test")
+public class AutoBeltGear extends LinearOpMode
+{
+    public DcMotor beltGearMotor = null;
+
+    @Override
+    public void runOpMode()
+    {
+        beltGearMotor = hardwareMap.get(DcMotor.class, "belt_gear_motor");
+        waitForStart();
+
+        while(opModeIsActive())
+        {
+            beltGearMotor.setPower(.4);
+        }
+
+    }
 }
